baafe39cf446e37f2eb40afbafed8b923e0cd4ba
Алексей <lehan@74.ru>
2016-11-26 16:56:23 +0600

    из двумерного в одномерный массив

--- source/alg_1.cpp
+++ source/alg_1.cpp
@@ -5,9 +5,10 @@
 
 using namespace std;
 
-bool** arr;//основной массив	
-bool** new_arr;//копия для расчёта след. шага
+bool* arr;//основной массив	
+bool* new_arr;//копия для расчёта след. шага
 int n, m, t;//n,m - размер массива, t - переменная колличества шагов
+int M, N;
 int num_threads = 4;
 
 void copy_array()//копирование массива new_arr в arr с учётом рамки
@@ -17,35 +18,37 @@ void copy_array()//копирование массива new_arr в arr с учётом рамки
 	{
 		for(int j = 1; j <= m; j++)
 		{
-			arr[i][j] = new_arr[i][j];
+			arr[i*M + j] = new_arr[i*M + j];
 		}
 	}
 
 	//правая и левая стороны (без углов)
 	for(int i = 1; i <= n; i++)
 	{
-		arr[i][0] = arr[i][m];
-		arr[i][m+1] = arr[i][1];
+		arr[i*M] = arr[i*M + m];
+		arr[i*M + m+1] = arr[i*M + 1];
 	}
 
 	//верхняя и нижняя стороны (без углов)
-	for(int i = 1; i <= m; i++)
+	for(int j = 1; j <= m; j++)
 	{
-		arr[0][i] = arr[n][i];
-		arr[n+1][i] = arr[1][i];
+		arr[j] = arr[n*M + j];
+		arr[(n+1)*M + j] = arr[M + j];
 	}
 
 	//углы
-	arr[0][0] = arr[n][m];
-	arr[0][m+1] = arr[n][1];
-	arr[n+1][0] = arr[1][m];
-	arr[n+1][m+1] = arr[1][1];
+	arr[0] = arr[n*M + m];
+	arr[m+1] = arr[n*M + 1];
+	arr[(n+1)*M] = arr[1*M + m];
+	arr[(n+1)*M + m+1] = arr[M + 1];
 }
 
 void read_data()//считывавем данные из файла и задаём разеры массива
 {
+	ifstream fin("cells_data.txt", ios_base::in);
+
 	int k, x, y;// k - колличество точек
-	cin >> n >> m >> num_threads >> t >> k;
+	fin >> n >> m >> num_threads >> t >> k;
 	
 	omp_set_dynamic(0);
 	omp_set_num_threads( num_threads );
@@ -63,25 +66,29 @@ void read_data()//считывавем данные из файла и задаём разеры массива
 			new_arr[i][j] = false;
 		}
 	}*/
-	arr = (bool **) _mm_malloc(((n+2) * sizeof (bool *)), 64);
-	new_arr = (bool **) _mm_malloc(((n+2) * sizeof (bool *)), 64);
+	N = n+2;
+	M = m+2;
+	new_arr = (bool *) _mm_malloc(((N)* (M) * sizeof (bool )), 64);
+	arr = (bool *) _mm_malloc(((N)* (M) * sizeof (bool )), 64);
+
     for (int i = 0; i < n+2; ++i)
 	{
-        arr[i] = (bool *) _mm_malloc(((m+2) * sizeof (bool)), 64);
-		new_arr[i] = (bool *) _mm_malloc(((m+2) * sizeof (bool)), 64);
 		for(int j = 0; j < m+2; j++)
 		{
-			arr[i][j] = false;
-			new_arr[i][j] = false;
+			arr[i*M + j] = false;
+			new_arr[i*M + j] = false;
 		}
 	}
 
+	
 	//считывание точек
 	for(int i = 0; i < k; i++)
 	{
-		cin >> x >> y;
-		new_arr[x][y] = true;
+		fin >> x >> y;
+		new_arr[x*(m+2) + y] = true;
 	}
+
+	fin.close();
 
 	//запускаем копирование
 	copy_array();
@@ -94,7 +101,7 @@ void read_data()//считывавем данные из файла и задаём разеры массива
 	{
 		for(int j = 0; j < m+2; j++)
 		{
-			if(arr[i][j])
+			if(arr[i*M + j])
 				cout << "0 ";
 			else
 				cout << ". ";
@@ -105,73 +112,64 @@ void read_data()//считывавем данные из файла и задаём разеры массива
 
 int main()
 {
-	freopen("cells_data.txt","r",stdin);
 	read_data();
 	double finish_time = 0;
 	double start_time = omp_get_wtime( );
 	//write_array();
-	#pragma omp parallel
-	{
-		for(int counter = 0; counter < t; counter++)
-		{	
-			#pragma omp for schedule(static)
-			for(int i = 1; i <= n; i++)
+	for(int counter = 0; counter < t; counter++)
+	{		
+		#pragma omp parallel for schedule(static)
+		for(int i = 0; i < n; i++)
+		{
+			#pragma omp simd
+			for(int j = 0; j < m; j++)
 			{
-				#pragma omp simd
-				for(int j = 1; j <= m; j++)
+				//ведём расчёт на ячейку i+1 j+1
+				int CurI = i*M+j; 
+				int k = arr[CurI] + arr[CurI+1] + arr[CurI + 2] + arr[CurI + M ] + arr[CurI + M + 2 ] + arr[CurI + M + M]  
+					+ arr[CurI + M + M + 1] + arr[CurI + M + M + 2] ;				
+				//k += arr[i+ki][j+kj];	
+				CurI = CurI + M + 1; 
+				if(arr[CurI] && k != 3 && k != 2)//умертвление
 				{
-					int k = 0;
-					if(arr[i][j+1]) k++;
-					if(arr[i+1][j]) k++;
-					if(arr[i][j-1]) k++;
-					if(arr[i-1][j]) k++;
-					if(arr[i+1][j+1]) k++;
-					if(arr[i-1][j+1]) k++;
-					if(arr[i+1][j-1]) k++;
-					if(arr[i-1][j-1]) k++;
-				
-					if(arr[i][j] && k != 3 && k != 2)//умертвление
-						new_arr[i][j] = false;
-					if(!arr[i][j] && k == 3)//создание
-						new_arr[i][j] = true;
-					//в других случаях ничего не меняем
+					new_arr[CurI] = false;
 				}
-			}
-			//копируем все наши действия в основной массив
-			//центральная (основная) часть
-			#pragma omp for schedule(static)
-			for(int i = 1; i <= n; i++)
-			{
-				#pragma omp simd
-				for(int j = 1; j <= m; j++)
+				if(!arr[CurI] && k == 3)//создание
 				{
-					arr[i][j] = new_arr[i][j];
+					new_arr[CurI] = true;
 				}
+				//в других случаях ничего не меняем
 			}
-
-			//правая и левая стороны (без углов)
-			#pragma omp for schedule(static) 
-			for(int i = 1; i <= n; i++)
-			{
-				arr[i][0] = arr[i][m];
-				arr[i][m+1] = arr[i][1];
-			}
-
-			//верхняя и нижняя стороны (без углов)
-			#pragma omp for schedule(static) 
-			for(int i = 1; i <= m; i++)
-			{
-				arr[0][i] = arr[n][i];
-				arr[n+1][i] = arr[1][i];
-			}
-
-			//углы
-			arr[0][0] = arr[n][m];
-			arr[0][m+1] = arr[n][1];
-			arr[n+1][0] = arr[1][m];
-			arr[n+1][m+1] = arr[1][1];
-		//	write_array();
 		}
+		//копируем все наши действия в основной массив
+		//центральная (основная) часть
+		#pragma omp parallel for schedule(static)
+		for(int i = 1; i <= N*M; i++)
+		{
+			arr[i] = new_arr[i];
+		}
+
+		//правая и левая стороны (без углов)
+		#pragma omp parallel for schedule(static)
+		for(int i = 1; i <= n; i++)
+		{
+			arr[i*M] = arr[i*M + m];
+			arr[i*M + m+1] = arr[i*M + 1];
+		}
+
+		//верхняя и нижняя стороны (без углов)
+		#pragma omp parallel for schedule(static)
+		for(int j = 1; j <= m; j++)
+		{
+			arr[j] = arr[n*M + j];
+			arr[(n+1)*M + j] = arr[M + j];
+		}
+		//углы
+		arr[0] = arr[n*M + m];
+		arr[m+1] = arr[n*M + 1];
+		arr[(n+1)*M] = arr[1*M + m];
+		arr[(n+1)*M + m+1] = arr[M + 1];
+		//write_array();
 	}
 	finish_time = omp_get_wtime( );
 	ofstream fout("alg_1_runtime.txt", ios_base::app);
@@ -179,13 +177,5 @@ int main()
 	fout.close();
 	//write_array();
 
-	//освобождение памяти
-    for (int i = 0; i < n+2; ++i)
-	{
-         _mm_free(arr[i]);
-		 _mm_free(new_arr[i]);
-	}
-	_mm_free(arr);
-	_mm_free(new_arr);
 	return 0;
 }
